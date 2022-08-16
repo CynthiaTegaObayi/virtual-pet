2ec2354342acf330a297299a90ada7ba1b347808
Cynthia Obayi <tegaobayi@gmail.com>
2022-08-16 22:05:36 +0100

    getting older

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -5,4 +5,11 @@ describe('constructor', () => {
 
     expect(pet.name).toEqual('Fido');
   });
+
+   
+it('has a initial age of 0', () => {
+  const pet = new Pet('Fido');
+
+  expect(pet.age).toEqual(0);
+});
 });
