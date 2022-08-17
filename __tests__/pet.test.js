193b5cb77af6048791541b76f84318e388bf1346
Cynthia Obayi <tegaobayi@gmail.com>
2022-08-17 21:38:44 +0100

    hunger

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -13,3 +13,26 @@ it('has a initial age of 0', () => {
   expect(pet.age).toEqual(0);
 });
 });
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+  it('has a initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+  
+    expect(pet.hunger).toEqual(0);
+  });
+  });
+  describe('growUp', () => {
+    it('increments hunger by 5', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.hunger).toEqual(5);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -2,8 +2,10 @@ const pet = new Pet();
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
     this.growUp = function() {
         this.age += 1;
+        this.hunger += 5;
     };
 }
 module.exports = Pet;
